dd7e4720d404c966fb21a4a546f7a07e93208fb6
TeraBasedProgrammer <ilyadronov2332@gmail.com>
2022-03-31 11:39:29 +0300

    some audio problems were corrected

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+let audioNL = document.querySelectorAll('audio');
+        let audio = Array.apply(null, audioNL);
+        audio.forEach(t => {
+            let index = audio.indexOf(t);
+
+            t.addEventListener('play', ()=> {
+                audio.forEach(subT => {
+                    subT !== audio[index] ?
+                    (subT.pause(), subT.currentTime = 0) :
+                    subT.play()
+                })
+            })
+            t.addEventListener('ended', ()=> {
+                t.currentTime = 0;
+                index !== audio.length - 1 ?
+                    audio[index + 1].play() :
+                    null
+            })
+        });
+
+/* Обработка scroll-up кнопки*/
+
 $(function() {
   $('.scrollup').click(function() {
     $("html, body").animate({
@@ -13,3 +35,7 @@ $(window).scroll(function() {
     $('.scrollup').fadeOut();
   }
 });
+
+/* ================================ */
+
+/* Код для корректной работы тега audio */
